6bdf5a372548099599b07d9f43000ba7ef667cc3
mawesome <kissisme@hotmail.com>
2021-02-03 08:16:45 -0800

    user schema

---
+++ models/user-model.js
@@ -0,0 +1,11 @@
+const mongoose = require("../connection");
+
+const UserSchema = mongoose.Schema({
+  username: String,
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  projects: Array,
+});
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
