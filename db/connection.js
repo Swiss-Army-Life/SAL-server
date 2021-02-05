c00bcfda6a4af2fbf431ee4eb52a32d16fd1a921
mawesome <kissisme@hotmail.com>
2021-02-05 08:38:35 -0800

    deploy

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const mongoURI =
   process.env.NODE_ENV === "production"
     ? process.env.DB_URL
-    : `mongodb://localhost:27017/projects`;
+    : `mongodb://localhost/projects`;
 mongoose
   .connect(mongoURI, {
     useNewUrlParser: true,
