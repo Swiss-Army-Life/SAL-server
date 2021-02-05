83f80c4bd98476b0e28619d47406b006b0049686
mawesome <kissisme@hotmail.com>
2021-02-05 09:20:18 -0800

    fixed port

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
 
 // routes --------
 
-app.set("port", 8000);
+app.set("port", process.env.PORT || 8000);
 app.listen(app.get("port"), () => {
   console.log(`PORT: ${app.get("port")}🌟`);
 });
